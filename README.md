c02ab3a11b79d05fe6e6987aa32a98b9b49ab110
leitz <leitzner.aguey@gmail.com>
2023-05-21 08:46:54 -0400

    extra validation on map tile requests, to use locally storaged images and improving README file instructions

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Local map tile cache, tries to request the image map tile and storages locally. Tested on Ubuntu 22.04 LTS
+Local map tile cache, tries to request the image map tile and storages locally to avoid making multiple requests to map tiles server like Openstret maps. Tested on Ubuntu 22.04 LTS.
+
+Please!!! note that each map tile provider haves its own legal usage policies, please enter to each web page provider to validate this.
 
 Installation:
 
@@ -16,6 +18,10 @@ In "areas/" directory are saved the tiles, optionally you can run a cron script 
 /usr/bin/rm ...your_path/maptilecache/areas/gmaps/osm/*
 
 If you want to make the script running permanently you must use some package like pm2.
+
+This script can be used with Leaflet Maps, using this tile format:
+
+http://x.x.x.x/osm?z={z}&x={x}&y={z}
 
 Any suggestion make it!
 
--- server.js
+++ server.js
@@ -14,26 +14,35 @@ app.get('/', (req, res) => {
 // http://x.x.x.x/osm?z=7&x=28&y=55
 // https://tile.openstreetmap.org/7/28/55.png
 app.get("/osm", async (request, response) => {
-   const arrayBuffer = await axios.get('https://tile.openstreetmap.org/' + request.query.z + '/' + request.query.x + '/' + request.query.y + '.png', {
-      responseType: 'arraybuffer'
-   });
+   // if no exists locally, request the map tile remotely
+   if (!fs.existsSync('./areas/osm/' + request.query.z + '_' + request.query.x + '_' + request.query.y + '.png')) {
+      const arrayBuffer = await axios.get('https://tile.openstreetmap.org/' + request.query.z + '/' + request.query.x + '/' + request.query.y + '.png', {
+         responseType: 'arraybuffer'
+      });
 
-   const buffer = Buffer.from(arrayBuffer.data, "base64");
-   fs.writeFileSync('./areas/osm/' + request.query.z + '_' + request.query.x + '_' + request.query.y + '.png', buffer);
+      const buffer = Buffer.from(arrayBuffer.data, "base64");
+      fs.writeFileSync('./areas/osm/' + request.query.z + '_' + request.query.x + '_' + request.query.y + '.png', buffer);
+   }
 
+   // return the map tile image
    response.sendFile(__dirname + '/areas/osm/' + request.query.z + '_' + request.query.x + '_' + request.query.y + '.png');
 });
 
 
 app.get("/mapquest", async (request, response) => {
-   const arrayBuffer = await axios.get('https://api.mapbox.com/styles/v1/mapquest/ck62b47z90j2j1iqglrzyrhtg/tiles/256/' + request.query.z + '/' + request.query.x + '/' + request.query.y + '@2x?access_token=' + MAPQUEST_KEY, {
-      responseType: 'arraybuffer'
-   });
+   // if no exists locally, request the map tile remotely
+   if (!fs.existsSync('./areas/mapquest/' + request.query.z + '_' + request.query.x + '_' + request.query.y + '.png')) {
+      const arrayBuffer = await axios.get('https://api.mapbox.com/styles/v1/mapquest/ck62b47z90j2j1iqglrzyrhtg/tiles/256/' + request.query.z + '/' + request.query.x + '/' + request.query.y + '@2x?access_token=' + MAPQUEST_KEY, {
+         responseType: 'arraybuffer'
+      });
 
-   const buffer = Buffer.from(arrayBuffer.data, "base64");
-   fs.writeFileSync('./areas/mapquest/' + request.query.z + '_' + request.query.x + '_' + request.query.y + '.png', buffer);
+      const buffer = Buffer.from(arrayBuffer.data, "base64");
+      fs.writeFileSync('./areas/mapquest/' + request.query.z + '_' + request.query.x + '_' + request.query.y + '.png', buffer);
+   }
 
+   // return the map tile image
    response.sendFile(__dirname + '/areas/mapquest/' + request.query.z + '_' + request.query.x + '_' + request.query.y + '.png');
 });
+
 
 app.listen(EXPRESS_SERVER_PORT);
